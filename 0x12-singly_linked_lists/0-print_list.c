ba80ba5cf1edada9c9a901eb92c467701fd49edc
AlainNkazamurego <alain.nkazamurego@gmail.com>
2023-04-06 04:42:20 +0200

    Space case no brake case

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,10 +10,9 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-
 	while (h != NULL)
 	{
-		if (h-> str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
@@ -21,6 +20,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		count++;
 	}
-
 	return (count);
 }

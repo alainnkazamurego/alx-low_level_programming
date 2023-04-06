91397ad09129ae544f916909ddea569abc6e4f6b
AlainNkazamurego <alain.nkazamurego@gmail.com>
2023-04-06 04:43:58 +0200

    While space

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,6 +10,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
+	
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -20,6 +21,6 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		count++;
 	}
-	
+
 	return (count);
 }

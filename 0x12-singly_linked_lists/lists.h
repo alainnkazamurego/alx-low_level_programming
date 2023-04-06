ca4810aca26a20a8a139936f07f1780add1b1aa6
AlainNkazamurego <alain.nkazamurego@gmail.com>
2023-04-06 04:29:28 +0200

    Last cheching

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef _LINKED_LIST
 #define _LINKED_LIST
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -7,12 +8,13 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for alx project
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _strlen(const char *s);
